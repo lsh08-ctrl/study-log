d0b820e5771031323b88963513d9cdf5a6f6ad1f
heon_lee <l_sh08@g.yju.ac.kr>
2026-09-10 12:22:30 +0900

    Hours Change

---
+++ Python/Function/2026_09_08_Hours_change.py
@@ -0,0 +1,14 @@
+# int(input()) 으로 정수 한 개를 읽습니다. 예: 입력이 "3661" 이면 sec == 3661
+sec = int(input())
+
+# to_hms 함수를 정의한다.
+def to_hms(sec):
+    # 시간, 분, 초 값을 반환한다.
+    h = sec // 3600
+    m = (sec % 3600) // 60
+    s = sec % 60
+    return h, m, s
+
+# 언패킹하여 출력한다.
+hours, minutes, seconds = to_hms(sec)
+print(hours, minutes, seconds)
