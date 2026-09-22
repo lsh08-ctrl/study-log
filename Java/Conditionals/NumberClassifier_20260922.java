b72268f4e1db10ccfe53edc1c6f5eaa09ee921d6
heon_lee <l_sh08@g.yju.ac.kr>
2026-09-22 11:18:43 +0900

    Feat: number classfier

---
+++ Java/Conditionals/NumberClassifier_20260922.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class NumberClassifier_20260922 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String value; 
+        
+        // 양수일 때
+        if (n > 0) {
+            // 양수이면서 짝수이면
+            if (n % 2 == 0) {
+                value = "양의 짝수";
+            }
+            // 양수이면서 홀수이면
+            else {
+                value = "양의 홀수";
+            }
+        }
+        // 양수가 아니면
+        else {
+            value = "양수 아님";
+        }
+        
+        // 결과 출력
+        System.out.println(value);
+        sc.close();
+    
+    }
+}
